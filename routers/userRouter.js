789865d5075dc3b474a9f5d40c9ad1d6490718d2
baekjung <zvgandam@gmail.com>
2019-06-26 13:04:41 +0900

    MVC Pattern part Three

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,13 +1,17 @@
 import express from "express";
 import routes from "../routes";
+import {
+  users,
+  userDeatil,
+  editProfile,
+  changePassword
+} from "../controllers/userController";
 
 const userRouter = express.Router();
 
-userRouter.get(routes.home, (req, res) => res.send("Users"));
-userRouter.get(routes.userDetail, (req, res) => res.send("User Detail"));
-userRouter.get(routes.editProfile, (req, res) => res.send("Edit Profile"));
-userRouter.get(routes.changePassword, (req, res) =>
-  res.send("Change Password")
-);
+userRouter.get(routes.home, users);
+userRouter.get(routes.userDetail, userDeatil);
+userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.changePassword, changePassword);
 
 export default userRouter;
