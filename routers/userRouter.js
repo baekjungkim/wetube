60f44a8438cff857aaaf2c84cceefa468d5c6be3
baekjung <zvgandam@gmail.com>
2019-06-26 16:26:54 +0900

    Add Pug template

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -9,7 +9,6 @@ import {
 
 const userRouter = express.Router();
 
-userRouter.get(routes.home, users);
 userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, changePassword);
 userRouter.get(routes.userDetail, userDeatil);
