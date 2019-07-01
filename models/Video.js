8ebd8f49cc5b578954ae54aa1ea6abcd41cb82b4
baekjung <zvgandam@gmail.com>
2019-07-01 11:07:28 +0900

    comment add Comment Model

--- models/Video.js
+++ models/Video.js
@@ -18,6 +18,7 @@ const VideoSchema = new mongoose.Schema({
     type: Date,
     default: Date.now
   },
+  //   Comment ID 를 배열로 저장하는 방법
   comments: [
     {
       type: mongoose.Schema.Types.ObjectId,
